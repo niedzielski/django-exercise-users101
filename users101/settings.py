a6a724dc80175f9ba7f1514d45d56fbcdcd9e036
Stephen Niedzielski <stephen@niedzielski.com>
2012-09-01 22:35:33 -0600

    Configure database.

--- users101/settings.py
+++ users101/settings.py
@@ -1,5 +1,8 @@
 # Django settings for users101 project.
 
+import os
+PROJECT_ROOT = os.path.join(os.path.dirname(__file__), os.path.pardir)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -11,8 +14,8 @@ MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, __package__ + '.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
